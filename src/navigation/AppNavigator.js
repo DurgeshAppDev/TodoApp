4f00e1655bc786b71fe99c70f9b8084ad5aebd19
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-04-01 19:44:43 +0530

    basic updates

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,13 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from '../screens/HomeScreen';
+import Profile from '../screens/Profile';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title:'My Todos'}} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{title:'Home'}} />
+        <Stack.Screen name="Profile" component={Profile} option={{title:'User Profile'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, TextInput, Button } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View className="flex-1 bg-white p-5 justify-center">
+      <Text className="text-center text-blue-400 text-4x1 font-bold mb-6" >
+        Home Screen
+      </Text>
+      <TextInput
+        className="border border-gray-300 rounded-lg p-3 mb-6 text-black  text-lg"
+        placeholder="enter your task"
+      />
+      <Button title="Profile" onPress={() => navigation.navigate('Profile')} />
     </View>
   );
 };
+
+export default HomeScreen;
