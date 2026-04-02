d395a19ff05f88b6276087f366fda73ecd1390ff
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-04-02 22:00:33 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
