4a914388325d3e103a74dcdce44c11f69169fb86
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-04-10 19:22:45 +0530

    fixed some issue

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import auth from '@react-native-firebase/auth';
 
 import SignInScreen from '../screens/SignInScreen';
 import LoginScreen from '../screens/LoginScreen';
@@ -8,30 +10,52 @@ import Profile from '../screens/Profile';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(user => {
+      console.log("Auth User:", user);
+      setUser(user);
+      setLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) return null;
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignInScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home' }}
-        />
+        {!user ? (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignInScreen}
+              options={{ headerShown: true }}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ title: 'Home' }}
+            />
 
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ title: 'User Profile' }}
-        />
+            <Stack.Screen
+              name="Profile"
+              component={Profile}
+              options={{ title: 'User Profile' }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
